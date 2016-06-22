9b17bfc795db2c43ca00bd9edea2a8d4b32ea82c
Utkarsh Malik <utkarshmalik211@gmail.com>
2016-06-22 14:13:58 +0530

    added validation method

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,4 @@
 class TodoList < ApplicationRecord
+  validates :title, presence: true
+  #validates :description, absence: true
 end
---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+
+describe "creating todo lists" do
+  it "redirects to the todo_lists on success" do
+    visit "/todo_lists"
+    click_link "New Todo List"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title",with:"hey"
+    fill_in "Description",with:"you are great"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("Todo list was successfully created.")
+  end
+  it "does'nt adds entry when title is blank" do
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    click_link "New Todo List"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title",with:""
+    fill_in "Description",with:"you are great"
+    click_button "Create Todo list"
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+  end
+end
